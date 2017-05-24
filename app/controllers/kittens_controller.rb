d0a600408aff27b84315ff5ee520a28a86de4875
peter <peto.mester@gmail.com>
2017-05-24 19:46:49 +0200

    Kittens Html App finish

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,52 @@
+class KittensController < ApplicationController
+  def new
+    @kitten = Kitten.new
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten created"
+      redirect_to @kitten
+    else
+      flash[:danger] = "Error"
+      render :new
+    end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update_attributes(kitten_params)
+      flash[:success] = "Kitten updated"
+      redirect_to @kitten
+    else
+      flash[:danger] = "Error"
+      render :edit
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def destroy
+    Kitten.find(params[:id]).destroy
+    flash[:success] = "Kitten deleted"
+    redirect_to root_path       
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+40.times do
+  Kitten.create!(name: Faker::Name.first_name, age: Random.rand(10) + 1,
+                cuteness: Random.rand(10) +1, softness: Random.rand(10) +1)
+
+
+end
